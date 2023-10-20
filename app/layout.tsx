9c7072a74b956f4c3854a4fb66910208b3afc566
amine <amine@alphorm.com>
2023-10-20 02:58:10 +0100

    update web layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             <div className='relative flex min-h-screen flex-col'>
             <SiteHeader />
-              <div className="flex lg:grid lg:grid-cols-5">
-                <div className="w-full lg:col-span-4 border-t-l">
+              <div className="flex">
+                <div className="w-full border-t-l">
                   <div className='flex-1'>{children}</div>
                 </div>
               </div>
